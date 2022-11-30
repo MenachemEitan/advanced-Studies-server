49f47d26a49b71a26d57fb7f78d927ffc6edab54
MenachemEitan <menahemeitan@gmail.com>
2022-11-30 17:39:24 +0200

    popularClass_debug

--- Routs/class.routs.js
+++ Routs/class.routs.js
@@ -5,68 +5,87 @@ const DB = require('../lib/dbControler');
 const question = new DB('question');
 const classes = new DB('class');
 const field = new DB('field')
-const {ErrItemAlreadyExists, ErrItemDoesntExist} = require('../lib/ResponseHandler')
+const class_rathing = new DB('class_reathing')
+const { ErrItemAlreadyExists, ErrItemDoesntExist } = require('../lib/ResponseHandler')
 
 
-route.post('/addfield',(req, res, next) =>{
-    const {fieldName} = req.body;
+route.post('/addfield', (req, res, next) => {
+    const { fieldName } = req.body;
     const existsField = field.getByFieldName(fieldName);
-    if (!existsField){
-       return res.ok(field.addItem(req.body))
-    }else
+    if (!existsField) {
+        return res.ok(field.addItem(req.body))
+    } else
         return res.not(ErrItemAlreadyExists("field"))
 
 })
 
-route.post('/addclass/:fieldName',(req, res, next) =>{
-    const {className} = req.body;
+route.post('/addclass/:fieldName', (req, res, next) => {
+    const { className } = req.body;
     const existsClass = classes.getByClassName(className);
-    if (!existsClass){
+    if (!existsClass) {
         tempClass = classes.addItem(req.body)
         tempField = field.getByFieldName(req.params.fieldName)
-        if(!tempField.class){
+        if (!tempField.class) {
             tempField.class = [tempClass.id]
-        } else{
+        } else {
             tempField.class = [...tempField.class, tempClass.id]
         }
-        field.updateItem(tempField.id,tempField )
-        
+        field.updateItem(tempField.id, tempField)
+
         return res.ok(tempClass)
-    }else
+    } else
         return res.not(ErrItemAlreadyExists("class"))
 })
 
-route.post('/addquestion/:className',(req, res, next) =>{
-    const {className} = req.params;
+route.post('/addquestion/:className', (req, res, next) => {
+    const { className } = req.params;
     const tempClass = classes.getByClassName(className);
     const tempQuestion = question.addItem(req.body);
-    if(!tempClass.question){
+    if (!tempClass.question) {
         tempClass.question = [tempQuestion.id]
-    }else{
-        tempClass.question = [...tempClass.question,tempQuestion.id]
+    } else {
+        tempClass.question = [...tempClass.question, tempQuestion.id]
     }
     classes.updateItem(tempClass.id, tempClass)
     return res.ok(tempQuestion)
 })
 
 
-route.get('/:id', (req, res, next)=>{
+route.get('/:id', (req, res, next) => {
     const classId = req.params.id;
     tempClass = classes.getById(classId)
-    if(tempClass){
+    if (tempClass) {
         return res.ok(tempClass)
-    }else
-    return res.not(ErrItemDoesntExist('class'))
+    } else
+        return res.not(ErrItemDoesntExist('class'))
 })
 
-route.get('/question/:id', (req, res, next)=>{
+route.get('/question/:id', (req, res, next) => {
     const questionId = req.params.id;
     tempQuestion = question.getById(questionId)
-    if(tempQuestion){
+    if (tempQuestion) {
         return res.ok(tempQuestion)
-    }else
-    return res.not(ErrItemDoesntExist('Question'))
+    } else
+        return res.not(ErrItemDoesntExist('Question'))
 })
+
+route.get('/popularClass/get', (req, res, next) => {
+    const temp = class_rathing.get()[0]
+
+    var items = Object.keys(temp).map(function (key) {
+        return [key, temp[key]];
+    });
+    items.sort(function (first, second) {
+        return second[1] - first[1];
+    });
+    let returnVal =[]
+    const tempList = items.slice(0, 5)
+
+    tempList.forEach(element =>returnVal.push(classes.getById(parseInt(element[0]))) )
+    return res.ok(returnVal)
+})
+
+
 
 
 

592a829da887519e621a53d36e2e096cc25d7f4b
MenachemEitan <menahemeitan@gmail.com>
2022-12-01 18:06:00 +0200

    recommended_route

--- Routs/users.routs.js
+++ Routs/users.routs.js
@@ -5,8 +5,10 @@ const { ErrItemAlreadyExists,ErrWrongPass,ErrItemDoesntExist, ErrNotAuth  } = re
 const route = express.Router();
 const app = express();
 const users = new DB('users');
+const classes = new DB('class')
 const {signUpSchema, logInSchema} = require('../dto/usersSchema');
 const validator = require('../dto/validator');
+const { object } = require('yup');
 
 
 route.post('/login', validator(logInSchema), (req, res, next)=>{
@@ -46,6 +48,40 @@ route.post('/signup',validator(signUpSchema) ,(req, res, next)=>{
         return res.ok(user.myClass)
     }
  })
+
+ route.get('/recommended',(req, res, next)=>{
+    if(!req.user){
+        return res.not(ErrNotAuth())
+    }else{
+        const user = users.getById(req.user);
+        const tempRecommendations = [];
+        classes.get().forEach(element => {
+            for(let val in element.connectivity){
+                if (user.myClass[val] && !user.myClass[element.id]){
+                    tempRecommendations.push([element.id,element.connectivity[val]])
+                }
+            }
+        });
+        tempCompress = {}
+        for(let i = 0; i<tempRecommendations.length;i++ ){
+            if(!(tempRecommendations[i][0] in tempCompress) ){
+                tempCompress[tempRecommendations[i][0]] = tempRecommendations[i][1]
+            }else{
+                tempCompress[tempRecommendations[i][0]] += tempRecommendations[i][1]
+            }
+        }
+        const items = Object.keys(tempCompress).map(function(key) {
+            return [key, tempCompress[key]];
+          });
+          items.sort(function(first, second) {
+            return second[1] - first[1];
+          });
+        res.ok(items.slice(0, 5))
+    }
+    
+ })
+
+
 
 
 
